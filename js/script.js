c52f37a5fbf009921d2ef4fb4af25d96499d5ba9
clarkrc82 <clarkrc82@gmail.com>
2018-09-30 21:50:34 -0700

    work work work
    Finshed the "other" input and t-shirt color selectors

--- js/script.js
+++ js/script.js
@@ -3,20 +3,30 @@ $("#name").focus();
 $('#other-title').hide();
 
 
-// $('#title').on('click', function(event){
-//   const $other = $('#title[option^="other"]');
-//     if('click'===$other){
-//   console.log(event.target);
-// }
-// 
-// });
-
-
-const $other =$('#title option').eq(5);
-
-$($other).on('click', function(){
-  console.log("hi");
+//Shows the Job Role input when "other" is selected 
+$('#title').change(function(){ 
+  if($(this).val() == "other"){
+    $('#other-title').show();
+  }
 });
+
+
+const $puns = $('#color option').filter(':eq(0), :eq(1), :eq(2)');
+const $hearts = $('#color option').filter(':eq(3), :eq(4), :eq(5)');
+
+$('#design').change(function(){
+  if($(this).val() === "heart js"){
+    $puns.hide();
+  }else{
+    $puns.show();
+  }
+  if($(this).val() === "js puns"){
+    $hearts.hide();
+  }else{
+    $hearts.show();
+  }
+});
+
 
 
 
