8bd5c23b7a8e9c13da2318d1c7138e63329056da
clarkrc82 <clarkrc82@gmail.com>
2018-10-02 12:50:25 -0700

    lots of changes

--- js/script.js
+++ js/script.js
@@ -7,14 +7,17 @@ $('#other-title').hide();
 $('#title').change(function(){ 
   if($(this).val() == "other"){
     $('#other-title').show();
+  }else{
+    $('#other-title').hide();
   }
 });
 
-
+// Variables for color options 
 const $puns = $('#color option').filter(':eq(0), :eq(1), :eq(2)');
 const $hearts = $('#color option').filter(':eq(3), :eq(4), :eq(5)');
 
-$('#design').change(function(){
+//Function that shows/hides the available color options by shirt type
+$('#design').on("change", function(){
   if($(this).val() === "heart js"){
     $puns.hide();
   }else{
@@ -28,9 +31,47 @@ $('#design').change(function(){
 });
 
 
+const $temp = $('<h2></h2>')
+$('.activities').append($temp);
 
 
+$('.activities').on('change', function () {
+    let $total = 0;
 
+  
+  if ($('input:checkbox:eq(0)').is(':checked')) {
+    $total += 200;
+  }
+  if ($('input:checkbox:eq(1)').is(':checked')) {
+    $('input:checkbox:eq(3)').prop('checked', false).parent().css('color', 'red').css('text-decoration', 'line-through');
+    $total += 100;
+  } else {
+    $('input:checkbox:eq(3)').parent().css('color', '').css('text-decoration', '');
+  }
+  if ($('input:checkbox:eq(2)').is(':checked')) {
+    $('input:checkbox:eq(4)').prop('checked', false).parent().css('color', 'red').css('text-decoration', 'line-through');
+    $total += 100;
+  } else {
+    $('input:checkbox:eq(4)').parent().css('color', '').css('text-decoration', '');
+  }
+  if ($('input:checkbox:eq(3)').is(':checked')) {
+    $('input:checkbox:eq(1)').prop('checked', false).parent().css('color', 'red').css('text-decoration', 'line-through');
+    $total += 100;
+  } else {
+    $('input:checkbox:eq(1)').parent().css('color', '').css('text-decoration', '');
+  }
+  if ($('input:checkbox:eq(4)').is(':checked')) {
+    $('input:checkbox:eq(2)').prop('checked', false).parent().css('color', 'red').css('text-decoration', 'line-through');
+    $total += 100;
+  } else {
+    $('input:checkbox:eq(2)').parent().css('color', '').css('text-decoration', '');
+  }
+  if ($('input:checkbox:eq(5)').is(':checked')) {
+    $total += 100;
+  }
+    if ($('input:checkbox:eq(6)').is(':checked')) {
+      $total += 100;
+    }
 
-
-
+  $('h2').text("Total: $"+$total);
+});
